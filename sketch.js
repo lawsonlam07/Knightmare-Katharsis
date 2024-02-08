59a1540fbef7efcbaf84803bc45e6ebd54411db8
Lawson Lam <109973600+Pararcana@users.noreply.github.com>
2024-02-08 08:28:26 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,6 @@ let newFEN = "rn1qkb1r/pp2pppp/2p2n2/5b2/P1pP3N/2N5/1P2PPPP/R1BQKB1R"
 let mouseBuffer = [false, false, false]
 let flip = true
 let turn = true
-
 let whiteLeftRook = true
 let whiteRightRook = true
 let blackLeftRook = true
