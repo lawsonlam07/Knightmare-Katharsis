0ecc2c5f1400d14093fd833799cd51204cb3c22a
Lawson Lam <109973600+Pararcana@users.noreply.github.com>
2024-02-10 09:12:12 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,6 @@
 let startFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
 let newFEN = "rn1qkb1r/pp2pppp/2p2n2/5b2/P1pP3N/2N5/1P2PPPP/R1BQKB1R"
 let knightOffsets = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]]
-
 let mouseBuffer = [false, false, false]
 let flip = true
 let mode = "game"
